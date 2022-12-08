eaca3565ae54accdca6e9db66050194a019cb666
AlexisTAE <alexis@taecontrol.com>
2022-12-08 16:55:25 -0400

    [#3] update serviceProvider position

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,18 +3,13 @@
 namespace Taecontrol\Larvis\Tests;
 
 use Orchestra\Testbench\TestCase as Orchestra;
-use Taecontrol\Larvis\Providers\LarvisServiceProvider;
+use  Taecontrol\Larvis\Providers\LarvisServiceProvider;
 
 class TestCase extends Orchestra
 {
     public function getEnvironmentSetUp($app)
     {
-        // config()->set('database.default', 'testing');
 
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_larastats-wingman_table.php.stub';
-        $migration->up();
-        */
     }
 
     protected function setUp(): void
