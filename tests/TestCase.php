076fa595e4f4501502bff17e1838fe6d95598a8d
Alexis TAE <117616772+AlexisTAE@users.noreply.github.com>
2022-12-09 10:11:24 -0400

    [#3] add run test github actions (#6)
    * [#3] add run test github actions
    
    * [#3] add description to composer json
    
    * [#3] add test script
    
    * [#3]  add more fixes to composer json
    
    * [#3] update composer lock
    
    * [#3] update composer
    
    * [#3
    ] add test examples
    
    * [#3] more updates
    
    * [#3] fix test command
    
    * [#3] update test script
    
    * [#3] add phpunit xml
    
    * [#3] update serviceProvider position
    
    * [#3] chance dir of service provider
    
    * [#3] update composer lock
    
    * [#3] more updates
    
    * [#3] fix directories in providers and namespaces
    
    * [#3] update composer lock
    
    * [#3] delete spatie package

---
+++ tests/TestCase.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Taecontrol\Larvis\Tests;
+
+use Orchestra\Testbench\TestCase as Orchestra;
+use Taecontrol\Larvis\Providers\LarvisServiceProvider;
+
+class TestCase extends Orchestra
+{
+    public function getEnvironmentSetUp($app)
+    {
+
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            LarvisServiceProvider::class,
+        ];
+    }
+}
