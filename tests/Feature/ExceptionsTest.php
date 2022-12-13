d612e8426c8aa05bc99cdd46fee894fccbb6e51a
AlexisTAE <alexis@taecontrol.com>
2022-12-13 17:38:27 -0400

    [#1] add exceptions test

---
+++ tests/Feature/ExceptionsTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Taecontrol\Larvis\Tests;
+
+use Exception;
+use Illuminate\Http\Client\Request;
+use Taecontrol\Larvis\Larvis;
+use Illuminate\Support\Facades\Http;
+
+class ExceptionsTest extends TestCase
+{
+    public function test_it_check_if_handler_function_post_extra_data()
+    {
+        $larvis = app(Larvis::class);
+        $exception = new Exception('exception');
+        $data = [
+            'name' => "test",
+            "key" => "akdflasjdfl"
+        ];
+        
+        config()->set("larvis.larastats.domain", "https://larastats.test");
+
+        Http::fake([
+            'https://larastats.test/*' => Http::response([], 200, []),
+        ]);
+        
+        $response = $larvis->captureException($exception, $data);
+        ray($response);
+
+        Http::assertSent(function (Request $request) use ($data) {
+            return $request['name'] == $data['name'] &&
+                   $request['key'] == $data['key'];
+        });
+    }
+}
