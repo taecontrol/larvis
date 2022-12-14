67caa432dda6f4cb2531959b4afb4391a662a229
AlexisTAE <alexis@taecontrol.com>
2022-12-14 10:53:12 -0400

    [#1] add test for check exception data

--- tests/Feature/ExceptionsTest.php
+++ tests/Feature/ExceptionsTest.php
@@ -6,6 +6,7 @@ use Exception;
 use Taecontrol\Larvis\Larvis;
 use Illuminate\Http\Client\Request;
 use Illuminate\Support\Facades\Http;
+use Taecontrol\Larvis\ValueObjects\ExceptionData;
 
 class ExceptionsTest extends TestCase
 {
@@ -26,7 +27,7 @@ class ExceptionsTest extends TestCase
             'https://larastats.test/*' => Http::response([], 200, []),
         ]);
 
-        $response = $larvis->captureException($exception, $data);
+        $larvis->captureException($exception, $data);
 
         Http::assertSent(function (Request $request) use ($data) {
             return $request['name'] == $data['name'] &&
@@ -34,9 +35,29 @@ class ExceptionsTest extends TestCase
         });
     }
     
-    // /** @test */
-    // public function check_if_exception_data_exists()
-    // {
+    /** @test */
+    public function check_if_exception_data_exists()
+    {
+        $larvis = app(Larvis::class);
+        $exception = new Exception('test exception');
+        $exceptionData = ExceptionData::from($exception)->toArray();
 
-    // } 
+        config()->set('larvis.larastats.domain', 'https://larastats.test');
+
+        Http::fake([
+            'https://larastats.test/*' => Http::response([], 200, []),
+        ]);
+
+        $larvis->captureException($exception, []);
+
+        Http::assertSent(function (Request $request) use($exceptionData) {
+            return $request["message"] == $exceptionData["message"] &&
+                   $request["type"] == $exceptionData["type"] &&
+                   $request["file"] == $exceptionData["file"] &&
+                   $request["line"] == $exceptionData["line"] &&
+                   $request["trace"] == $exceptionData["trace"] &&
+                   $request["request"] == $exceptionData["request"];
+        });
+    } 
 }
+
