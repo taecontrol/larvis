16b5fa92b038a47e3d3bcfa53da732539b95b1e5
Ronald Pereira <rpereira@taecontrol.com>
2023-06-28 11:32:03 -0400

    Fix json payload size on request->responses

--- src/ValueObjects/Data/ResponseData.php
+++ src/ValueObjects/Data/ResponseData.php
@@ -2,7 +2,11 @@
 
 namespace Taecontrol\Larvis\ValueObjects\Data;
 
+use Illuminate\View\View;
+use Illuminate\Support\Str;
 use Illuminate\Http\Response;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Contracts\Support\Arrayable;
 
 class ResponseData implements Arrayable
@@ -11,7 +15,7 @@ class ResponseData implements Arrayable
         public readonly int $status,
         public readonly string $statusText,
         public readonly array $headers,
-        public readonly bool|string $content,
+        public readonly array|string $content,
         public readonly string $version,
         public readonly mixed $original,
     ) {
@@ -19,11 +23,13 @@ class ResponseData implements Arrayable
 
     public static function from(Response $response): ResponseData
     {
+        $content = static::formatContent($response);
+
         return new self(
             status: $response->getStatusCode(),
             statusText: $response->statusText(),
             headers: $response->headers->all(),
-            content: $response->getContent(),
+            content: $content,
             version: $response->getProtocolVersion(),
             original: $response->getOriginalContent(),
         );
@@ -41,6 +47,35 @@ class ResponseData implements Arrayable
         ];
     }
 
+    public static function formatContent(Response $response): array|string
+    {
+        $content = $response->getContent();
+
+        if (is_string($content)) {
+            if (is_array(json_decode($content, true)) &&
+                json_last_error() === JSON_ERROR_NONE) {
+                return json_decode($content, true);
+            }
+
+            if (Str::startsWith(strtolower($response->headers->get('Content-Type')), 'text/plain')) {
+                return $content;
+            }
+        }
+
+        if ($response instanceof RedirectResponse) {
+            return 'Redirected to ' . $response->getTargetUrl();
+        }
+
+        if ($response instanceof Response && $response->getOriginalContent() instanceof View) {
+            return [
+                'view' => $response->getOriginalContent()->getPath(),
+                'data' => static::extractDataFromView($response->getOriginalContent()),
+            ];
+        }
+
+        return 'HTML Response';
+    }
+
     public static function fromArray(array $args): ResponseData
     {
         return new ResponseData(
@@ -55,13 +90,39 @@ class ResponseData implements Arrayable
 
     public function debugFormat(): array
     {
+        $debugContent = $this->content;
+
+        if(is_array($this->content)) {
+            $debugContent = json_encode($this->content);
+        }
+
         return [
             'status' => strval($this->status),
             'status_text' => $this->statusText,
             'headers' => json_encode($this->headers),
-            'content' => strval($this->content),
+            'content' => $debugContent,
             'version' => $this->version,
             'original' => json_encode($this->original),
         ];
+    }
+
+    protected static function extractDataFromView(View $view)
+    {
+        return collect($view->getData())
+            ->map(function ($value) {
+                if ($value instanceof Model) {
+                    return $value->toArray();
+                }
+
+                if (is_object($value)) {
+                    return [
+                        'class' => get_class($value),
+                        'properties' => json_decode(json_encode($value), true),
+                    ];
+                }
+
+                return json_decode(json_encode($value), true);
+            })
+            ->toArray();
     }
 }
