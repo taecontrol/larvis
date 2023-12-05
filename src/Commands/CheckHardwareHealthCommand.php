3e93018808f4a3ed1692ec8ef5a97c859e5c4732
AlexisTAE <alexis@taecontrol.com>
2023-12-05 12:19:34 -0400

    [#82] add hardwareService update command

--- src/Commands/CheckHardwareHealthCommand.php
+++ src/Commands/CheckHardwareHealthCommand.php
@@ -3,10 +3,8 @@
 namespace Taecontrol\Larvis\Commands;
 
 use Illuminate\Console\Command;
-use Taecontrol\Larvis\Exceptions\CpuHealthException;
-use Taecontrol\Larvis\Exceptions\DiskHealthException;
-use Taecontrol\Larvis\Exceptions\MemoryHealthException;
 use Taecontrol\Larvis\Larvis;
+use Taecontrol\Larvis\Services\HardwareService;
 
 class CheckHardwareHealthCommand extends Command
 {
@@ -14,76 +12,14 @@ class CheckHardwareHealthCommand extends Command
 
     protected $description = 'check Cpu Memory and Disk space';
 
-
-    public function getHardDiskUsage()
-    {
-        $result = false;
-        $freeDiskSpace = false;
-        $totalDiskSpace = false;
-
-        if(function_exists('disk_free_space') && function_exists('disk_total_space')) {
-            $freeDiskSpace = round((disk_free_space('/') / pow(1024, 3)), 2);
-            $totalDiskSpace = round((disk_total_space('/') / pow(1024, 3)), 2);
-        }
-
-        $result = [
-            "freeDiskSpace" => $freeDiskSpace,
-            "totalDiskSpace" => $totalDiskSpace,
-        ];
-
-        if (!$result) {
-            throw DiskHealthException::class;
-        }
-
-        return $result;
-    }
-
-    public function getMemoryUsage()
-    {
-        $result = false;
-
-        if (function_exists('exec')) {
-            $memory = shell_exec(" free | grep Mem | awk '{print $3/$2 * 100}' ");
-            //$result = $memory;
-            $result = round((float)$memory, 2);  //correguir
-
-        }
-
-        if (!$result) {
-            throw MemoryHealthException::class;
-        }
-
-        return $result;
-
-    }
-
-    public function getCpuLoadUsage()
-    {
-        $result = false;
-
-        if (function_exists('sys_getloadavg')) {
-            $result = sys_getloadavg();
-        }
-
-        if (!$result) {
-            throw CpuHealthException::class;
-        }
-
-
-        $result = array_map(fn ($n) => round($n, 2), $result);
-
-        return $result[1]; //los ultimos 5 min
-    }
-
-
     public function handle()
     {
         /** @var Larvis */
         //$larvis = app(Larvis::class);
 
-        $test = $this->getCpuLoadUsage();
-        $memory = $this->getMemoryUsage();
-        $disk = $this->getHardDiskUsage();
+        $test =  HardwareService::getCpuLoadUsage();
+        $memory = HardwareService::getMemoryUsage();
+        $disk = HardwareService::getDiskUsage();
 
         $this->info('cpu load');
         var_dump($test);
--- src/Services/HardwareService.php
+++ src/Services/HardwareService.php
@@ -2,8 +2,70 @@
 
 namespace Taecontrol\Larvis\Services;
 
+use Taecontrol\Larvis\Exceptions\CpuHealthException;
+use Taecontrol\Larvis\Exceptions\DiskHealthException;
+use Taecontrol\Larvis\Exceptions\MemoryHealthException;
 
 class HardwareService
 {
 
+    public static function getDiskUsage()
+    {
+        $result = false;
+        $freeDiskSpace = false;
+        $totalDiskSpace = false;
+
+        if(function_exists('disk_free_space') && function_exists('disk_total_space')) {
+            $freeDiskSpace = round((disk_free_space('/') / pow(1024, 3)), 2);
+            $totalDiskSpace = round((disk_total_space('/') / pow(1024, 3)), 2);
+        }
+
+        $result = [
+            "freeDiskSpace" => $freeDiskSpace,
+            "totalDiskSpace" => $totalDiskSpace,
+        ];
+
+        if (!$result) {
+            throw DiskHealthException::class;
+        }
+
+        return $result;
+    }
+
+    public static function getMemoryUsage()
+    {
+        $result = false;
+
+        if (function_exists('exec')) {
+            $memory = shell_exec(" free | grep Mem | awk '{print $3/$2 * 100}' ");
+            //$result = $memory;
+            $result = round((float)$memory, 2);  //correguir
+
+        }
+
+        if (!$result) {
+            throw MemoryHealthException::class;
+        }
+
+        return $result;
+
+    }
+
+    public static function getCpuLoadUsage()
+    {
+        $result = false;
+
+        if (function_exists('sys_getloadavg')) {
+            $result = sys_getloadavg();
+        }
+
+        if (!$result) {
+            throw CpuHealthException::class;
+        }
+
+
+        $result = array_map(fn ($n) => round($n, 2), $result);
+
+        return $result[1]; //los ultimos 5 min
+    }
 }
