ee319e37f2b5b1e15e778df874283947b5bbbc98
AlexisTAE <alexis@taecontrol.com>
2023-12-08 00:29:19 -0400

    [#82] update test

--- tests/Feature/Commands/CheckHardwareHealthTest.php
+++ tests/Feature/Commands/CheckHardwareHealthTest.php
@@ -4,7 +4,9 @@ namespace Taecontrol\Larvis\Tests\Feature\Commands;
 
 use Illuminate\Http\Client\Request;
 use Illuminate\Support\Facades\Http;
+use Mockery;
 use Taecontrol\Larvis\Larvis;
+use Taecontrol\Larvis\Services\HardwareService;
 use Taecontrol\Larvis\Tests\TestCase;
 
 class CheckHardwareHealthTest extends TestCase
@@ -15,9 +17,20 @@ class CheckHardwareHealthTest extends TestCase
 
         config()->set('larvis.moonguard.url', 'http://localhost:58673');
         config()->set('larvis.moonguard.api.hardware', '/moonguard/api/hardware');
-    }
 
-    public function test_console_command(): void
+        // mock hardware service
+        $hardwareServiceMock = Mockery::mock(HardwareService::class);
+        $hardwareServiceMock->shouldReceive('getCPULoadUsage')->once()->andReturn(10.2);
+        $hardwareServiceMock->shouldReceive('getMemoryUsage')->once()->andReturn(23);
+        $hardwareServiceMock->shouldReceive('getDiskUsage')->once()->andReturn([
+            'freeSpace' => 79.8,
+            'totalSpace' => 181.7,
+        ]);
+
+        app()->instance(HardwareService::class, $hardwareServiceMock);
+    }
+    /** @test */
+    public function it_asserts_that_data_its_send_correctly(): void
     {
         /** @var Larvis */
         app(Larvis::class);
@@ -28,17 +41,17 @@ class CheckHardwareHealthTest extends TestCase
             'disk' => [
                 'freeSpace' => 79.8,
                 'totalSpace' => 181.7
-            ]
+            ],
+            'api_token' => config('larvis.moonguard.site.api_token'),
         ];
 
         Http::fake(['https://localhost:5873/*' => Http::response(null, 201, [])]);
 
-        $this->artisan('check:hardware')->expectsOutput('data')->assertExitCode(0);
+        $this->artisan('check:hardware')->run();
 
         Http::assertSent(function (Request $request) use ($data) {
-            var_dump($request);
-
-            return $request === $data['disk']['totalSpace'];
+            dd($request);
+            return $request === $data;
         });
     }
 }
